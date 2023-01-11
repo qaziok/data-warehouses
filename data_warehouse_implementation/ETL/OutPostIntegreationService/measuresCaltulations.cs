687afaa57e08e6f5d5be03b551899d4336771aa1
StanislawSm <105853049+StanislawSm@users.noreply.github.com>
2023-01-11 14:29:49 +0100

    lol

--- data_warehouse_implementation/ETL/OutPostIntegreationService/measuresCaltulations.cs
+++ data_warehouse_implementation/ETL/OutPostIntegreationService/measuresCaltulations.cs
@@ -6,16 +6,16 @@ private double prevLat;
 
 
 DateTime rowDateTime = Row.Data.Add(Row.Czas);
-if (prevId == Row.IDzlecenia)
-{
-	Row.czasrealizacji = (prevDateTime - rowDateTime).TotalSeconds;
-	double lat1 = Row.Geolokalizacjaszerokosc;
-	double lon1 = Row.Geolokalizacjadlugosc;
-	double lat2 = prevLat;
-	double lon2 = prevLon;
-	Row.pokonanaodleglosc = Math.Acos(Math.Sin(lat1) * Math.Sin(lat2) + Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(lon2 - lon1)) * 6371000;
-}
-prevDateTime = rowDateTime;
-prevId = Row.IDzlecenia;
-prevLat = Row.Geolokalizacjaszerokosc;
-prevLon = Row.Geolokalizacjadlugosc;
+        if (prevId == Row.IDzlecenia)
+        {
+            Row.czasrealizacji = (prevDateTime - rowDateTime).TotalSeconds;
+            double lat1 = Row.Geolokalizacjaszerokosc * 2 * Math.PI / 360;
+            double lon1 = Row.Geolokalizacjadlugosc * 2 * Math.PI / 360;
+            double lat2 = prevLat;
+            double lon2 = prevLon;
+            Row.pokonanaodleglosc = Math.Acos(Math.Sin(lat1) * Math.Sin(lat2) + Math.Cos(lat1) * Math.Cos(lat2) * Math.Cos(lon2 - lon1)) * 6371000;
+        }
+        prevDateTime = rowDateTime;
+        prevId = Row.IDzlecenia;
+        prevLat = Row.Geolokalizacjaszerokosc * 2 * Math.PI /360;
+        prevLon = Row.Geolokalizacjadlugosc * 2 * Math.PI / 360;
